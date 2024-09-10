41b3fc6277e0428216428b433a20b101ff54315c
rxgx424 <gkfudrd1234@gmail.com>
2024-09-10 11:15:10 +0900

    Please enter the commit message for your changes. Lines starting 	modified:   frontend/node_modules/.bin/acorn 	modified:   frontend/node_modules/.bin/ansi-html 	modified:   frontend/node_modules/.bin/autoprefixer 	modified:   frontend/node_modules/.bin/browserslist 	modified:   frontend/node_modules/.bin/cssesc 	modified:   frontend/node_modules/.bin/eslint 	modified:   frontend/node_modules/.bin/esparse 	modified:   frontend/node_modules/.bin/esvalidate 	modified:   frontend/node_modules/.bin/flat 	modified:   frontend/node_modules/.bin/he 	modified:   frontend/node_modules/.bin/highlight 	modified:   frontend/node_modules/.bin/html-minifier-terser 	modified:   frontend/node_modules/.bin/is-ci 	modified:   frontend/node_modules/.bin/is-docker 	modified:   frontend/node_modules/.bin/js-yaml 	modified:   frontend/node_modules/.bin/jsesc 	modified:   frontend/node_modules/.bin/json5 	modified:   frontend/node_modules/.bin/mime 	modified:   frontend/node_modules/.bin/mkdirp 	modified:   frontend/node_modules/.bin/multicast-dns 	modified:   frontend/node_modules/.bin/nanoid 	modified:   frontend/node_modules/.bin/opener 	modified:   frontend/node_modules/.bin/parser 	modified:   frontend/node_modules/.bin/prettier 	modified:   frontend/node_modules/.bin/regjsparser 	modified:   frontend/node_modules/.bin/resolve 	modified:   frontend/node_modules/.bin/rimraf 	modified:   frontend/node_modules/.bin/semver 	modified:   frontend/node_modules/.bin/svgo 	modified:   frontend/node_modules/.bin/terser 	modified:   frontend/node_modules/.bin/update-browserslist-db 	modified:   frontend/node_modules/.bin/uuid 	modified:   frontend/node_modules/.bin/vue-cli-service 	modified:   frontend/node_modules/.bin/webpack 	modified:   frontend/node_modules/.bin/webpack-bundle-analyzer 	modified:   frontend/node_modules/.bin/webpack-dev-server 	modified:   frontend/node_modules/.bin/which 	modified:   frontend/node_modules/.cache/eslint/af4f82ae.json 	modified:   frontend/node_modules/@vue/babel-preset-app/node_modules/.bin/semver 	modified:   frontend/node_modules/@vue/cli-shared-utils/node_modules/.bin/semver 	modified:   frontend/node_modules/cross-spawn/node_modules/.bin/semver 	modified:   frontend/node_modules/css-loader/node_modules/.bin/semver 	modified:   frontend/node_modules/default-gateway/node_modules/.bin/node-which 	modified:   frontend/node_modules/eslint-plugin-vue/node_modules/.bin/semver 	modified:   frontend/node_modules/eslint/node_modules/.bin/node-which 	modified:   frontend/node_modules/eslint/node_modules/.bin/semver 	modified:   frontend/node_modules/espree/node_modules/.bin/acorn 	modified:   frontend/node_modules/loader-utils/node_modules/.bin/json5 	modified:   frontend/node_modules/normalize-package-data/node_modules/.bin/semver 	modified:   frontend/node_modules/postcss-loader/node_modules/.bin/semver 	modified:   frontend/node_modules/regjsparser/node_modules/.bin/jsesc 	modified:   frontend/node_modules/vue-eslint-parser/node_modules/.bin/semver 	modified:   frontend/src/App.vue 	new file:   frontend/src/assets/Farmi.svg 	new file:   frontend/src/assets/menu.svg 	modified:   frontend/src/components/AppChatbot.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,21 @@
 <template> <!--HTML짜는 곳-->
-  <aside class="menu" v-if="show">
-    <div>
-      <button id="hidden" @click="toggleShow"></button>
-    </div>
-    <div id="function">
-      <p class="bg-red"><strong>기능들</strong></p>
-      <ul>
-        <li>챗봇</li>
-        <li>병해충진단</li>
+  <header><img src="./assets/Farmi.svg" alt="farmi" height="50vh"></header>
+  <nav class="menu">
+    <button id="menu-button" v-if="isMobile" @click="toggleShow"></button>
+    <div :id="{'nav-mobile': show}" >
+      <ul v-if="show">
+        <li><a href="#"></a>메인페이지</li>
+        <li><a href="#"></a>병해진단</li>
+        <li><a href="#"></a>챗봇</li>
+        <li><a href="#"></a>환경그래프</li>
       </ul>
     </div>
     <div id="search_list">
-      <p class="bg-red"><strong>검색 기록</strong></p>
     </div>
-  </aside>
-  <aside class="no-menu" v-else-if="!show">
-    <button id=show @click="toggleShow"></button>
-  </aside>
+  </nav>
   <main class="main">
     <div>
-      <button id="settings"></button>
+      <button id="search-list"></button>
     </div>
     <div id="chat">
       <div class="search">
@@ -38,12 +34,24 @@ export default{
   data(){
     return {
       show: true,
+      isMobile: false,
+      menuVisible: false 
     }
   },
   methods: {
     toggleShow(){
-      this.show=!this.show;
+      this.show =! this.show;
+    },
+    checkIfMobile(){
+      this.isMobile=window.innerWidth < 768;
     }
+  },
+  mounted() {
+    this.checkIfMobile();
+    window.addEventListener('resize', this.checkIfMobile);
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.checkIfMobile);
   },
   components: {
     AppChatbot
@@ -55,11 +63,22 @@ export default{
 <style> /*CSS짜는 곳*/
 body, html {
     margin: 0;
-    display: flex;
     height: 100vh;
 }
-p {
-  font-size: 1.5rem;
+header {
+  border-bottom: 2px solid black;
+  padding-left: 1.5vw;
+}
+ul {
+  /*list-style-type: none;*/
+  font-size: 125%;
+}
+li {
+  padding: 5px;
+}
+a {
+  text-decoration: none;
+  color: inherit;
 }
 button {
   width: 40px;
@@ -70,16 +89,15 @@ button {
 }
 .search{
   position: absolute;
+  justify-content: center;
   bottom: 5vh;
-  left: 30vw;
+  border: 2px solid blue;
 }
 .menu, .main {
   display: inline-block;
 }
 .menu {
   float: left;
-  width: 17.5vw;
-  height: 100vh;
   background-color: seashell;
 }
 .main {
@@ -91,22 +109,18 @@ button {
   color: white;
   background-color: #F99E17;
 }
-#hidden {
+#menu-button {
   float: right;
   top: 5px;
-  background-image: url('./assets/log-out.svg');
+  background-image: url('./assets/menu.svg');
 }
 #show {
   background-image: url('./assets/log-in.svg');
 }
-#function, #search_list, #chat {
-  padding: 50px 0px;
-  color: black;
-}
-#settings {
+#search-list {
   position: fixed;
   top: 5px;
   right: 20px;
-  background-image: url('./assets/settings.svg');
+  background-image: url('./assets/settings.svg'); /*검색기록 이미지 찾기*/
 }
 </style>
