920efb88a906df24b179ba90690ecf36fd637ce6
yujeong <dabc3166@naver.com>
2024-10-18 16:33:17 +0900

    modified:   frontend/src/components/DiseaseDiagnosis.vue

--- frontend/src/components/DiseaseDiagnosis.vue
+++ frontend/src/components/DiseaseDiagnosis.vue
@@ -30,14 +30,20 @@
     <div v-if="showDiagnosis" class="modal-background-Diagnosis">
         <div class="modal-Diagnosis">
             <button class="close-button" @click="closeDiagnosis"></button><br>
-            <h3>진단 결과</h3>
+            <h2>진단 결과</h2>
             <div class="modal-p">
+                <!-- <img :src="selectedPhoto" class="diagnosis-image" alt="진단 결과 이미지" /> -->
+                <img :src="diagnosis.resultImage" class="diagnosis-image" alt="diagnosis-image" />
                 <p><b>병명:</b> {{ diagnosis.disease }}</p>
                 <p><b>솔루션:</b> {{ diagnosis.solution }}</p>
             </div>
         </div>
     </div>
 
+    <!-- 로딩 화면 -->
+    <div v-if="isLoading" class="loading-overlay">
+        <p>병해진단 중입니다... 잠시만 기다려 주세요.</p>
+    </div>
 </template>
 
 <script>
@@ -50,11 +56,15 @@ export default {
             ShowModal: false,
             photos: [], // 업로드된 사진을 저장하는 배열
             showDiagnosis: false, // 진단 결과 모달창 상태
+            isLoading: false, // 로딩 상태
             diagnosis: {
                 disease: '',  //진단 결과의 병명
-                solution: ''  //진단 결과의 솔루션
+                solution: '',  //진단 결과의 솔루션
+                resultImage: '',    // 인공지능 진단 결과 이미지
             },
             isMenuOpen: false,
+
+            //selectedPhoto: '', // 선택된 업로드 사진(test)
         }
     },
     components: {
@@ -85,9 +95,12 @@ export default {
             }
         },
         // test용
-        // openDiagnosis() {
+        // openDiagnosis(photo) {
+        //     this.selectedPhoto = photo;
         //     try {
         //         this.diagnosis.disease = "곰팡이";
+
+
         //         this.diagnosis.solution = "ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ";
         //         this.showDiagnosis = true;  // 모달 열기
         //     } catch (error) {
@@ -95,11 +108,13 @@ export default {
         //         alert('진단 중 오류가 발생했습니다.');
         //     }
         // },
+
+        // 병해진단 수행
         async openDiagnosis(photo) {
             try {
-                // 파일을 FormData로 변환
-                const formData = new FormData();
-                formData.append('photo', photo.file);
+                this.isLoading = true;                 // 로딩 화면 표시
+                const formData = new FormData();       // 파일을 FormData로 변환
+                formData.append('photo', photo.file);  // 파일 객체 전송
 
                 // 서버에 진단 요청 보내기
                 const response = await axios.post('/api/diagnosis', formData, {
@@ -107,15 +122,16 @@ export default {
                         'Content-Type': 'multipart/form-data',
                     },
                 });
-
                 // 서버에서 받은 진단 결과 처리
                 this.diagnosis.disease = response.data.disease;
                 this.diagnosis.solution = response.data.solution;
+                this.diagnosis.resultImage = response.data.resultImage;
                 this.showDiagnosis = true;  // 모달 열기
-
             } catch (error) {
                 console.error('진단 중 오류 발생:', error);
                 alert('진단 중 오류가 발생했습니다.');
+            } finally {
+                this.isLoading = false;  // 로딩 화면 숨기기
             }
         },
         closeDiagnosis() {
@@ -140,7 +156,6 @@ export default {
     width: 37vw;
     height: 20%;
     transform: transform 0.1s ease;
-    /* 이동할 때 부드럽게 */
 }
 
 .Selection {
@@ -163,11 +178,9 @@ export default {
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
-    /* 배경 어둡게 */
     display: flex;
     justify-content: center;
     align-items: center;
-    /* 중앙 정렬 */
 }
 
 .modal-background-Diagnosis {
@@ -177,11 +190,9 @@ export default {
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
-    /* 배경 어둡게 */
     display: flex;
     justify-content: center;
     align-items: center;
-    /* 중앙 정렬 */
 }
 
 .modal {
@@ -206,22 +217,21 @@ export default {
     cursor: pointer;
 }
 
-.modal-Diagnosis h3 {
-    margin-bottom: 13px;
+.modal-Diagnosis h2 {
+    color: #FA782D;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #ddd;
 }
 
 .modal-Diagnosis p {
-    margin-bottom: 7px;
+    margin-top: 7px;
 }
 
 .modal-p {
     margin: 10px;
     max-height: 300px;
-    /* 스크롤이 생길 최대 높이 설정 */
     overflow-y: auto;
-    /* 세로 스크롤 활성화 */
     padding-right: 10px;
-    /* 스크롤바와 텍스트 사이 여백 */
     box-sizing: border-box;
 }
 
@@ -239,6 +249,31 @@ export default {
     border-radius: 10px;
 }
 
+.diagnosis-image {
+    width: 90%;
+    /* 가로 */
+    height: 208px;
+    /* 세로 */
+    object-fit: contain;
+    margin-right: 10px;
+    border-radius: 5px;
+}
+
+.loading-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    z-index: 9999;
+}
+
 .close-button {
     background-image: url('@/assets/closebutton.svg');
     background-size: contain;
@@ -249,25 +284,17 @@ export default {
     border: none;
     cursor: pointer;
     position: absolute;
-    /* 절대 위치로 설정 */
     top: 8px;
-    /* 모달의 상단에 붙이기 */
     right: 8px;
-    /* 모달의 오른쪽에 붙이기 */
 }
-
 
 /* li 태그 스타일 (버튼처럼 보이게) */
 .modal li {
     list-style: none;
-    /* 기본 리스트 스타일 제거 */
     padding: 10px 0;
-    /* 항목마다 여백 추가 */
     border-bottom: 1px solid #ddd;
-    /* 항목 구분선 */
     cursor: pointer;
     font-size: 1.25rem;
-    /* 글자 크기 증가 */
 }
 
 /* 마지막 항목은 구분선을 제거 */
@@ -278,24 +305,17 @@ export default {
 .photo-gallery {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-    /* 이미지가 화면에 맞게 정렬되도록 */
     gap: 8px;
-    /* 이미지 간의 간격을 설정 */
     margin-top: 20px;
-    /* 상단 여백 */
     max-height: calc(100vh - 200px);
-    /* 화면 높이에서 메뉴바 높이를 뺀 크기를 지정 */
     overflow-y: auto;
-    /* 세로로 스크롤이 생기도록 설정 */
     padding-right: 10px;
-    /* 스크롤바로 인한 오른쪽 여백 */
 }
 
 /* 이미지 미리보기 스타일 */
 .preview-image {
     width: 150px;
     height: 200px;
-    /* 이미지의 가로세로 비율을 유지하면서 자동으로 크기 조절 */
     object-fit: contain;
     border-radius: 5px;
     cursor: pointer;
