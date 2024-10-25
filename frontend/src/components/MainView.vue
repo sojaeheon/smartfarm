c1e179bd4e0e62ce6e6d727646e91bb141b2caba
sojaeheon <119103469+sojaeheon@users.noreply.github.com>
2024-10-25 11:38:05 +0900

    Update MainView.vue

--- frontend/src/components/MainView.vue
+++ frontend/src/components/MainView.vue
@@ -55,6 +55,7 @@ import AppHeader from '../components/AppHeader.vue';
 export default {
     data() {
         return {
+            videoSrc: 'http://202.31.150.31:9999/video_feed',
             actuators: [
                 { label: 'DC팬', isOn: false, imgSrc: require('../assets/dcfan.svg') },
                 { label: '워터펌프', isOn: false, imgSrc: require('../assets/water-pump.svg') },
