064a56a2c17cfdc7c1efe9e13b6e83b4f6e7076b
sojaeheon <sojaeheon123@daum.net>
2024-10-28 18:13:17 +0900

    modified:   frontend/dist/index.html 	modified:   frontend/src/components/LoginView.vue 	new file:   frontend/src/routor/auth.js 	modified:   frontend/src/routor/index.js 	new file:   frontend/src/services/authService.js

--- frontend/src/routor/index.js
+++ frontend/src/routor/index.js
@@ -5,6 +5,7 @@ import LoginView from "@/components/LoginView.vue";
 import MainView from "@/components/MainView.vue";
 import DiseaseDiagnosis from "@/components/DiseaseDiagnosis.vue";
 import AppChatbot from "@/components/AppChatbot.vue";
+import store from "@/routor/auth"; // Vuex 스토어를 가져옵니다.
 
 const router = createRouter({
     history: createWebHistory(),
@@ -40,5 +41,22 @@ const router = createRouter({
     ],
 });
 
+<<<<<<< HEAD
+=======
+// 네비게이션 가드 추가
+router.beforeEach((to, from, next) => {
+    const isAuthenticated = store.state.auth.loggedIn; // 로그인 상태 확인
+  
+    if (to.meta.requiresAuth && !isAuthenticated) {
+      // 인증이 필요한 경로에 접근하려고 할 때 로그인하지 않은 경우
+      next({ name: 'Login' });
+    } else if (to.name === 'Login' && isAuthenticated) {
+      // 로그인 상태인데 로그인 페이지로 이동하려고 할 때
+      next({ name: 'Main' });
+    } else {
+      next(); // 다음 라우트로 이동
+    }
+});
+>>>>>>> e20ad392 (	modified:   frontend/dist/index.html)
 
 export default router;
