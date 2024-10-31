f03f4a31ed7511dabeb8afb9b436433e03bd3962
sojaeheon <sojaeheon123@daum.net>
2024-10-31 19:38:03 +0900

    modified:   backend/env/lib/python3.12/site-packages/flask_cors/__pycache__/__init__.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/flask_cors/__pycache__/core.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/flask_cors/__pycache__/decorator.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/flask_cors/__pycache__/extension.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/flask_cors/__pycache__/version.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/markupsafe/__pycache__/__init__.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/__pycache__/__init__.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/__pycache__/_auth.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/__pycache__/charset.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/__pycache__/connections.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/__pycache__/converters.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/__pycache__/cursors.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/__pycache__/err.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/__pycache__/optionfile.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/__pycache__/protocol.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/constants/__pycache__/CR.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/pymysql/constants/__pycache__/ER.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/werkzeug/__pycache__/formparser.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/werkzeug/__pycache__/http.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/werkzeug/__pycache__/security.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/werkzeug/__pycache__/serving.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/werkzeug/debug/__pycache__/__init__.cpython-312.pyc 	modified:   backend/env/lib/python3.12/site-packages/werkzeug/sansio/__pycache__/multipart.cpython-312.pyc 	modified:   backend/myproject.py 	modified:   frontend/dist/index.html 	modified:   frontend/src/main.js 	modified:   frontend/src/store/store.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/store'; // Vuex 스토어 임포트
-import axios from 'axios'
+import axios from 'axios';
 
 // Axios 기본 URL 설정
-axios.defaults.baseURL = 'http://localhost:7000'; // 혹은 서버 IP를 넣을 수 있습니다.
+axios.defaults.baseURL = 'http://202.31.150.31';
 
 const app = createApp(App)
 app.use(router)
