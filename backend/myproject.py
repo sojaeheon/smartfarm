cd8df9c5e7438d97e085f90279c3018728f9ac67
sojaeheon <sojaeheon123@daum.net>
2024-11-01 16:35:24 +0900

    modified:   backend/myproject.py 	modified:   docker-compose.yml 	modified:   frontend/dist/index.html

--- backend/myproject.py
+++ backend/myproject.py
@@ -114,70 +114,13 @@ def logout():
     session.clear()
     return jsonify({"success": True, "message": "Logged out successfully."})
 
+          
 # 센서데이터
 @app.route('/api/sensor_data', methods=['POST'])
 def sensor_data():
     print(request.get_json())
 
-
-@app.route('/api/get_answer', methods=['GET', 'POST'])
-def get_answer():
-    if request.method == 'GET':
-        return jsonify({"message": "이 엔드포인트는 POST 메소드를 사용하여 질문을 보내야 합니다."}), 200
-
-    if request.method == 'POST':
-        data = request.get_json()
-        question = data.get('question')
-        print(question)
-        print(chain)
-        if not question:
-            return jsonify({"error": "질문이 제공되지 않았습니다."}), 400
-
-        try:
-            answer = get_answer_from_chain(chain, question)
-            print(answer,flush=True)
-                        
-            return jsonify({"answer": answer}), 200
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-        
-        
-@app.route('/api/disease', methods=['GET', 'POST'])
-def disease():
-    if request.method == 'GET':
-        return jsonify({"message": "이 엔드포인트는 POST 메소드를 사용하여 이미지를 보내야 합니다."}), 200
-
-    if request.method == 'POST':
-        if 'photo' not in request.files:
-            return jsonify({"error": "이미지가 제공되지 않았습니다."}), 400
-        
-        image = request.files['photo']
-        try:
-            # 이미지 파일을 임시로 저장
-            with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as tmp_file:
-                tmp_file.write(image.read())
-                tmp_file_path = tmp_file.name
-
-            # 모델 예측 수행
-            disease_name,__ = sb_decision(tmp_file_path)
-            question = f'딸기 {disease_name} 치료방법을 알려주세요.'
-            solution = get_answer_from_chain(chain, question)
-
-            print(question)
-            print(solution)
-            data = {
-                "disease": disease_name,
-                "solution": solution
-            }
-            print(data)
-            return jsonify(data), 200
-        except Exception as e:
-            return jsonify({"error": f"질병 진단 처리 중 오류 발생: {str(e)}"}), 500
-        finally:
-            os.remove(tmp_file_path)
-            
 if __name__ == "__main__":
-    init()
     app.run(host='0.0.0.0', port=7000, debug=True)
 
 
